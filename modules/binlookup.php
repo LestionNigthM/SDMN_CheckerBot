b67596477c59f22a6cf17ae6841a7792b6fd5be9
LestionNigthM <99926253+LestionNigthM@users.noreply.github.com>
2022-02-18 00:26:58 -0600

    Update binlookup.php

--- modules/binlookup.php
+++ modules/binlookup.php
@@ -21,7 +21,7 @@ if(strpos($message, "/bin ") === 0 || strpos($message, "!bin ") === 0){
     $antispam = antispamCheck($userId);
     addUser($userId);
     
-    if($antispam != False){
+    if($antispam != True){
       bot('sendmessage',[
         'chat_id'=>$chat_id,
         'text'=>"[<u>ANTI SPAM</u>] Try again after <b>$antispam</b>s.",
